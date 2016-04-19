337ab98d5a6b1343186506cddf5fcbe283c1f3ad
star <skyvtars@126.com>
2016-04-19 23:32:30 +0800

    支付宝支付

--- src/com/zangcun/store/activity/OrderActivity.java
+++ src/com/zangcun/store/activity/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.zangcun.store.activity;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
@@ -10,6 +11,7 @@ import android.os.Message;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +25,8 @@ import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListene
 import com.zangcun.store.BaseActivity;
 import com.zangcun.store.R;
 import com.zangcun.store.adapter.ChooseShopCarAdapter;
+import com.zangcun.store.alipay.AliPayUtil;
+import com.zangcun.store.alipay.PayResult;
 import com.zangcun.store.model.FxModel;
 import com.zangcun.store.model.GetAddressResultModel;
 import com.zangcun.store.model.ShopCarModel;
@@ -202,11 +206,21 @@ public class OrderActivity extends BaseActivity implements OnClickListener {
                 mWeixin.setSelected(true);
                 break;
             case R.id.pup_go_pay://去支付
-                if (mZFB.isSelected() || mWeixin.isSelected()) {
+                if (mZFB.isSelected()) {//支付宝支付
+                    try {
+                        AliPayUtil aliPayUtil = new AliPayUtil(this,mHandler);
+                        aliPayUtil.pay(0.01);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                     return;
-                } else {
-                    payDialog();
                 }
+                if (mWeixin.isSelected()) {//微信支付
+                    return;
+                }
+
+                    payDialog();
+
                 break;
         }
     }
@@ -314,6 +328,47 @@ public class OrderActivity extends BaseActivity implements OnClickListener {
 
             }
         }
+    };
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        @SuppressWarnings("unused")
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 1: {
+                    PayResult payResult = new PayResult((String) msg.obj);
+                    /**
+                     * 同步返回的结果必须放置到服务端进行验证（验证的规则请看https://doc.open.alipay.com/doc2/
+                     * detail.htm?spm=0.0.0.0.xdvAU6&treeId=59&articleId=103665&
+                     * docType=1) 建议商户依赖异步通知
+                     */
+                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
+
+                    String resultStatus = payResult.getResultStatus();
+                    // 判断resultStatus 为“9000”则代表支付成功，具体状态码代表含义可参考接口文档
+                    if (TextUtils.equals(resultStatus, "9000")) {
+                        Toast.makeText(OrderActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+                        // TODO: 2016/4/19 支付成功跳转界面
+//                        finish();
+                    } else {
+                        // 判断resultStatus 为非"9000"则代表可能支付失败
+                        // "8000"代表支付结果因为支付渠道原因或者系统原因还在等待支付结果确认，最终交易是否成功以服务端异步通知为准（小概率状态）
+                        if (TextUtils.equals(resultStatus, "8000")) {
+                            Toast.makeText(OrderActivity.this, "支付结果确认中", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // 其他值就可以判断为支付失败，包括用户主动取消支付，或者系统返回的错误
+                            Toast.makeText(OrderActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                    mPopWindow.dismiss();
+                    break;
+                }
+                default:
+                    break;
+            }
+        };
     };
 
 }
